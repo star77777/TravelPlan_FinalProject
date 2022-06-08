0d9ca070a22d1b70d51c527163a42b446b5a8ff2
star77777 <104746184+star77777@users.noreply.github.com>
2022-06-08 12:13:19 +0900

    #1 로그인 xml 과 회원가입 기능 구현

---
+++ app/src/main/java/com/example/travelplan_finalproject/ui/main/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.travelplan_finalproject.ui.main
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.travelplan_finalproject.BaseActivity
+import com.example.travelplan_finalproject.R
+import com.example.travelplan_finalproject.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity() {
+    lateinit var binding : ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+        binding.loginBtn.setOnClickListener {
+
+        }
+        binding.signUpBtn.setOnClickListener {
+
+        }
+    }
+
+    override fun setValues() {
+
+    }
+}
